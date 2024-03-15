d8f9eb83be7e541a46de1269d134e01a793be543
cedricngoune <gabyngoune@yahoo.fr>
2024-03-15 17:04:14 +0100

    create event using nats and abble to receive event when creating new ticket

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,9 +1,20 @@
 import mongoConnect from "./db/mongo-connect";
 
 import { app } from "./app";
+import { natsWrapper } from "./nats-wrapper";
 const port = 3000;
 
 const start = async () => {
+  await natsWrapper.connect("ticketing", "myClientId", "http://nats-srv:4222");
+
+  // Close connection to NATS
+  natsWrapper.client.on("close", () => {
+    console.log("Nats connection closed");
+    process.exit();
+  });
+  process.on("SIGINT", () => natsWrapper.client.close());
+  process.on("SIGTERM", () => natsWrapper.client.close());
+
   await mongoConnect;
   app.listen(port, () => {
     console.log(`Server running at ${port} 🚀`);
