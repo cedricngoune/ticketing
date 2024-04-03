1f6e3759de41a09a21a292e5a90b73f5b041f264
cedricngoune <gabyngoune@yahoo.fr>
2024-04-03 14:21:26 +0200

    nextjs: new commit

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
+import StripeCheckout from "react-stripe-checkout";
 
-const OrderShow = ({ order }) => {
+const OrderShow = ({ order, currentUser }) => {
   const [timeLeft, setTimeLeft] = useState(0);
 
   useEffect(() => {
@@ -20,7 +21,17 @@ const OrderShow = ({ order }) => {
     return <div>Order expired</div>;
   }
 
-  return <div>Time left to pay: {timeLeft} seconds</div>;
+  return (
+    <div>
+      Time left to pay: {timeLeft} seconds
+      <StripeCheckout
+        token={(token) => console.log(token)}
+        stripeKey="pk_test_51OyxIiP3DZAp8Ow1vUaN1COWjjHYnX4IMqsiCa70VM08tXuJJU0bUY2FrXmSLArLPD9SicBzx60QGkLVTGgeUkWy00e6sAx1rO"
+        amount={order.ticket.price * 100}
+        email={currentUser.email}
+      />
+    </div>
+  );
 };
 
 OrderShow.getInitialProps = async (context, client) => {
